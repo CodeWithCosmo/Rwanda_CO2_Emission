f65e4ecc41e1b5431ce6399128de18f3f6d47a1d
Lalit Singh Bisht <lb5470272@gmail.com>
2023-09-01 11:25:37 +0530

    Project Completion Phase 1

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Rwanda_CO2_Emission
-CO_2 Emission in Rwanda Predictions
+# `Titanic Disaster Survivor Prediction Model`
+* ## Applications Used
+1. [Python 3.8](https://www.python.org/)
+2. [Jupyter Notebook](https://jupyter.org/)
+3. [VSCodeIDE](https://code.visualstudio.com/)
+4. [GitCLI](https://git-scm.com/book/en/v2/Getting-Started-The-Command-Line)
+5. [Github Account](https://github.com)
+6. [Docker](https://www.docker.com/)
+7. [ThunderClient](https://www.thunderclient.com/)
+
+
+* ## Create a new environment
+
+```
+conda create --name venv python=3.8 -y
+```
+* ## Activate the newly created environment
+
+```
+conda activate venv/
+```
+* ## Install the requirements
+
+```
+pip install -r requirements.txt
+```
+
---
+++ app.py
@@ -0,0 +1,27 @@
+import pickle
+import numpy as np
+from flask import Flask,request,app,render_template
+
+app=Flask(__name__)
+
+##! Loading the model
+clf_model=pickle.load(open('classifier.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=clf_model.predict(final_input)[0]
+    if output == 1:
+        return render_template("home.html",prediction_text="This passenger was survived.".format(output))
+    else:
+        return render_template("home.html",prediction_text="This passenger was not survived.".format(output))
+
+if __name__=="__main__":
+    app.run(debug=True)
